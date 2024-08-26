e5e661202fb92196584be961c01990895c7d1963
vincehientran <35750052+vincehientran@users.noreply.github.com>
2024-08-25 20:59:55 -0500

    enable google sign in

--- app/src/main/java/com/cowbytegames/spellshade/Activities/LoginActivity.kt
+++ app/src/main/java/com/cowbytegames/spellshade/Activities/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.credentials.CredentialManager
@@ -27,7 +28,7 @@ import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
 import kotlinx.coroutines.launch
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : ComponentActivity() {
     private lateinit var googleBtn: Button
 
 
@@ -37,53 +38,53 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
     }
 
-//    fun handleSignIn(result: GetCredentialResponse) {
-//        val credential = result.credential
-//
-//        when (credential) {
-//            is CustomCredential -> {
-//                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-//                    try {
-//                        val googleIdTokenCredential = GoogleIdTokenCredential
-//                            .createFrom(credential.data)
-//                        val googleIdToken = googleIdTokenCredential.id
-//
-//                        Toast.makeText(this, "Google ID: $googleIdToken", Toast.LENGTH_LONG).show()
-//
-//                    } catch (e: GoogleIdTokenParsingException) {
-//                        Log.e(TAG, "Received an invalid google id token response", e)
-//                    }
-//                }
-//                else {
-//                    Log.e(TAG, "Unexpected type of credential")
-//                }
-//            }
-//            else -> {
-//                Log.e(TAG, "Unexpected type of credential")
-//            }
-//        }
-//    }
+    fun handleSignIn(result: GetCredentialResponse) {
+        val credential = result.credential
+
+        when (credential) {
+            is CustomCredential -> {
+                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential = GoogleIdTokenCredential
+                            .createFrom(credential.data)
+                        val googleIdToken = googleIdTokenCredential.id
+
+                        Toast.makeText(this, "Google ID: $googleIdToken", Toast.LENGTH_LONG).show()
+
+                    } catch (e: GoogleIdTokenParsingException) {
+                        Log.e(TAG, "Received an invalid google id token response", e)
+                    }
+                }
+                else {
+                    Log.e(TAG, "Unexpected type of credential")
+                }
+            }
+            else -> {
+                Log.e(TAG, "Unexpected type of credential")
+            }
+        }
+    }
 
     fun buttonGoogleSignIn(view: View) {
-//        val credentialManager = CredentialManager.create(this@LoginActivity)
-//
-//        val signInWithGoogleOption: GetSignInWithGoogleOption = GetSignInWithGoogleOption.Builder(getString(R.string.web_client_id)).build()
-//        val request: GetCredentialRequest = GetCredentialRequest.Builder()
-//            .addCredentialOption(signInWithGoogleOption)
-//            .build()
-//
-//
-//        lifecycleScope.launch {
-//            try {
-//                val result = credentialManager.getCredential(
-//                    request = request,
-//                    context = this@LoginActivity,
-//                )
-//                handleSignIn(result)
-//            } catch (e: GetCredentialException) {
-//                Log.e(TAG, "GetCredentialException")
-//            }
-//        }
+        val credentialManager = CredentialManager.create(this@LoginActivity)
+
+        val signInWithGoogleOption: GetSignInWithGoogleOption = GetSignInWithGoogleOption.Builder(getString(R.string.web_client_id)).build()
+        val request: GetCredentialRequest = GetCredentialRequest.Builder()
+            .addCredentialOption(signInWithGoogleOption)
+            .build()
+
+
+        lifecycleScope.launch {
+            try {
+                val result = credentialManager.getCredential(
+                    request = request,
+                    context = this@LoginActivity,
+                )
+                handleSignIn(result)
+            } catch (e: GetCredentialException) {
+                Log.e(TAG, "GetCredentialException")
+            }
+        }
     }
 
     fun buttonPlayOffline(view: View) {
