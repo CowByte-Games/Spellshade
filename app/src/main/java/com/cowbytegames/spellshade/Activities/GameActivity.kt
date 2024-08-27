5a8e62daf3cdbfa70668d41710cfd0839efa30af
vincehientran <35750052+vincehientran@users.noreply.github.com>
2024-08-26 22:29:07 -0500

    reorganize game activity

--- app/src/main/java/com/cowbytegames/spellshade/Activities/GameActivity.kt
+++ app/src/main/java/com/cowbytegames/spellshade/Activities/GameActivity.kt
@@ -15,6 +15,7 @@ import com.cowbytegames.spellshade.R
 
 class GameActivity : ComponentActivity() {
     private lateinit var board: Board
+    private lateinit var coordinateTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +23,60 @@ class GameActivity : ComponentActivity() {
 
         renderLayout()
 
-        val coordinateTextView = findViewById<TextView>(R.id.textView2)
+        coordinateTextView = findViewById<TextView>(R.id.textView2)
+        setPieceOnClick()
+    }
 
+    private fun renderLayout() {
+        setContentView(R.layout.activity_game)
+        renderBoardBackground()
+        renderPieces()
+    }
+
+    private fun renderBoardBackground() {
+        val boardView = findViewById<View>(R.id.board)
+        val rootView = findViewById<ViewGroup>(R.id.root_view)
+
+        rootView.post {
+            val marginInDp = 5
+            val marginInPx = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, marginInDp.toFloat(), resources.displayMetrics
+            ).toInt()
+
+            val width = rootView.width - 2 * marginInPx
+            val height = rootView.height - 2 * marginInPx
+            val size = minOf(width, height)
+
+            val layoutParams = boardView.layoutParams
+            layoutParams.width = size
+            layoutParams.height = size
+            boardView.layoutParams = layoutParams
+
+            val constraintLayout = rootView as ConstraintLayout
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(constraintLayout)
+
+            constraintSet.clear(boardView.id, ConstraintSet.TOP)
+            constraintSet.clear(boardView.id, ConstraintSet.START)
+            constraintSet.clear(boardView.id, ConstraintSet.END)
+            constraintSet.clear(boardView.id, ConstraintSet.BOTTOM)
+
+            constraintSet.connect(boardView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, marginInPx)
+            constraintSet.connect(boardView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, marginInPx)
+            constraintSet.connect(boardView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, marginInPx)
+
+            constraintSet.applyTo(constraintLayout)
+        }
+    }
+
+    private fun renderPieces() {
+        val gridLayout = findViewById<GridLayout>(R.id.board_grid)
+        val boardView = findViewById<ImageView>(R.id.board)
+
+        board.renderBoard(gridLayout, boardView)
+    }
+
+    private fun setPieceOnClick() {
         val imageViews: Array<ImageView> = arrayOf(
             findViewById(R.id.square_00),
             findViewById(R.id.square_01),
@@ -90,54 +143,5 @@ class GameActivity : ComponentActivity() {
                 coordinateTextView.text = coordinates
             }
         }
-    }
-
-    private fun renderLayout() {
-        setContentView(R.layout.activity_game)
-        renderBoardBackground()
-        renderPieces()
-    }
-
-    private fun renderBoardBackground() {
-        val boardView = findViewById<View>(R.id.board)
-        val rootView = findViewById<ViewGroup>(R.id.root_view)
-
-        rootView.post {
-            val marginInDp = 5
-            val marginInPx = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, marginInDp.toFloat(), resources.displayMetrics
-            ).toInt()
-
-            val width = rootView.width - 2 * marginInPx
-            val height = rootView.height - 2 * marginInPx
-            val size = minOf(width, height)
-
-            val layoutParams = boardView.layoutParams
-            layoutParams.width = size
-            layoutParams.height = size
-            boardView.layoutParams = layoutParams
-
-            val constraintLayout = rootView as ConstraintLayout
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(constraintLayout)
-
-            constraintSet.clear(boardView.id, ConstraintSet.TOP)
-            constraintSet.clear(boardView.id, ConstraintSet.START)
-            constraintSet.clear(boardView.id, ConstraintSet.END)
-            constraintSet.clear(boardView.id, ConstraintSet.BOTTOM)
-
-            constraintSet.connect(boardView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, marginInPx)
-            constraintSet.connect(boardView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, marginInPx)
-            constraintSet.connect(boardView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, marginInPx)
-
-            constraintSet.applyTo(constraintLayout)
-        }
-    }
-
-    private fun renderPieces() {
-        val gridLayout = findViewById<GridLayout>(R.id.board_grid)
-        val boardView = findViewById<ImageView>(R.id.board)
-
-        board.renderBoard(gridLayout, boardView)
     }
 }
