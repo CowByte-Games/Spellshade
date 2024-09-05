0dbb5d7e8300c63dcbe651a4511b29eda3c7a65e
Pearleen Tran <76570762+pearltran080@users.noreply.github.com>
2024-09-05 18:45:19 -0500

    added end game message

--- app/src/main/java/com/cowbytegames/spellshade/Game/Game.kt
+++ app/src/main/java/com/cowbytegames/spellshade/Game/Game.kt
@@ -1,7 +1,9 @@
 package com.cowbytegames.spellshade.Game
 
 import android.widget.TextView
+import android.widget.Toast
 import com.cowbytegames.spellshade.Game.Pieces.*
+import com.cowbytegames.spellshade.Game.Pieces.Common.Piece
 import kotlinx.coroutines.*
 
 class Game(val board: Board, val narratorTextView: TextView, val actionPointTextView: TextView, private val doneProcessingCallBack: () -> Unit,){
@@ -27,10 +29,60 @@ class Game(val board: Board, val narratorTextView: TextView, val actionPointText
             delay(3000)
             resetDebuffs()
             board.renderPieces()
+            delay(1000)
+            isEndGame()
 
             board.flipActivePlayer()
             actionPointTextView.text = "Action Points: ${board.getActionPoints()}"
             doneProcessingCallBack()
+        }
+    }
+
+    private fun isEndGame() {
+        val attackers = listOf(Wizard::class, Warrior::class, Archer::class, Assassin::class)
+
+        var blueAttackers = 0
+        var redAttackers = 0
+        var blueCommander = false
+        var redCommander = false
+
+        var piece : Piece? = null
+
+        for (i in 0 until 7) {
+            for (j in 0 until 7) {
+                piece = board.get(i, j)
+                if (piece == null) {
+                    continue
+                }
+                if (piece::class in attackers) {
+                    if (piece.player == 1) {
+                        blueAttackers += 1
+                    } else {
+                        redAttackers += 1
+                    }
+                }
+                else if (piece is Commander) {
+                    if (piece.player == 1) {
+                        blueCommander = true
+                    } else {
+                        redCommander = true
+                    }
+                }
+            }
+        }
+
+        if (blueCommander && !redCommander) {
+            narratorTextView.text = "BLUE TEAM WINS"
+        } else if (!blueCommander && redCommander) {
+            narratorTextView.text = "RED TEAM WINS"
+        } else if (!blueCommander && !redCommander) {
+            narratorTextView.text = "DRAW"
+        } else if (blueAttackers > 0 && redAttackers == 0) {
+            narratorTextView.text = "BLUE TEAM WINS"
+        } else if (blueAttackers == 0 && redAttackers > 0) {
+            narratorTextView.text = "RED TEAM WINS"
+        } else if (blueAttackers == 0 && redAttackers == 0) {
+            narratorTextView.text = "STALEM8"
         }
     }
 
